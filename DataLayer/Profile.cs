5074bf44eafbb146627372972677fdcb8bc08818
Guday <Guday@DESKTOP-Q190V7K>
2024-04-13 08:31:26 +0800

    Add project files.

---
+++ DataLayer/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+
+
+namespace DataLayer
+{
+    public class Profile
+    {
+        public void DisplayGroup(Group group)
+        {
+            Console.WriteLine("Group Name: " + group.Name);
+            Console.WriteLine("Group Members: " + group.Members);
+            Console.WriteLine("Debut Date: " + group.DebutDate);
+            Console.WriteLine("Company: " + group.Company);
+        }
+    }
+}
---
+++ Model/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace Model
+{
+    public class Group
+    {
+        public string Name;
+        public string Members;
+        public string DebutDate;
+        public string Company;
+    }
+}
